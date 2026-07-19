53f5a08efa4651eb31692ea868c74d91f608e23e
Álvaro Alencar <ac.alvaro@gmail.com>
2026-07-19 12:23:19 -0300

    Configura Nearby Connections e versão 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.alvaro.ruidobranco"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
@@ -24,4 +24,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-nearby:19.3.0")
 }
